5b307e55c3c0e9fd452a9723d6468d7fcc76b977
lesh <lesh@sysphere.org>
2016-07-07 09:25:53 +0200

    ugly quick changes to color the syntax and support RGB/RGBA formats

--- colorpicker.el
+++ colorpicker.el
@@ -58,6 +58,176 @@
           (goto-char (car bounds))
           (delete-region (point) (cdr bounds)))
         (insert picked-color)))))
+(defun xah-syntax-color-hex ()
+  "Syntax color hex color spec such as 「#ff1100」 in current buffer."
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   '(("#[abcdef[:digit:]]\\{6\\}"
+      (0 (put-text-property
+          (+ (match-beginning 0) 1)
+          (match-end 0)
+          'face (list :background (match-string-no-properties 0)))))))
+  (font-lock-fontify-buffer)
+  )
+
+
+(defun xah-syntax-color-hsl ()
+  "Syntax color CSS's HSL color spec ➢ for example: 「hsl(0,90%,41%)」 in current buffer.
+URL `http://ergoemacs.org/emacs/emacs_CSS_colors.html'
+Version 2015-06-11"
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   '(("hsl( *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\)% *, *\\([0-9]\\{1,3\\}\\)% *)"
+      (0 (put-text-property
+          (+ (match-beginning 0) 3)
+          (match-end 0)
+          'face
+          (list
+           :background
+           (concat
+            "#"
+            (mapconcat
+             'identity
+             (mapcar
+              (lambda (x) (format "%02x" (round (* x 255))))
+              (color-hsl-to-rgb
+               (/ (string-to-number (match-string-no-properties 1)) 360.0)
+               (/ (string-to-number (match-string-no-properties 2)) 100.0)
+               (/ (string-to-number (match-string-no-properties 3)) 100.0)))
+             "" )) ;  "#00aa00"
+           ))))))
+  (font-lock-fontify-buffer))
+
+
+
+
+(defun syntax-color-rgb ()
+  "Syntax color CSS's RGB color spec ➢ for example: 「rgb(0,90,41)」 in current buffer."
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   '(("rgb( *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\) *)"
+      (0 (put-text-property
+          (+ (match-beginning 0) 4)
+          (match-end 3)
+          'face
+          (rgb-to-face
+           (list
+               (string-to-number (match-string-no-properties 1))
+               (string-to-number (match-string-no-properties 2))
+               (string-to-number (match-string-no-properties 3))
+               ))            
+          )))))
+  (font-lock-fontify-buffer))
+
+
+(defun xah-syntax-color-rgba ()
+  "Syntax color CSS's RGBa color spec ➢ for example: 「rgba(0,90,41,0.2)」 in current buffer."
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   '(("rgba( *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\),.*\)"
+      (0 (put-text-property
+          (+ (match-beginning 0) 5)
+          (match-end 3)
+          'face
+          (rgb-to-face
+           (list
+               (string-to-number (match-string-no-properties 1))
+               (string-to-number (match-string-no-properties 2))
+               (string-to-number (match-string-no-properties 3))
+               ))            
+          )))))
+  (font-lock-fontify-buffer))
+
+
+(defun rgb-to-face (rgb)
+    (interactive)
+(list
+ :background
+ (concat "#" (mapconcat 'identity (mapcar (lambda (x) (format "%02x" x)) rgb) "" ))
+ :box
+ "#0076FC"
+; :foreground
+; (message (concat "#" (mapconcat 'identity (mapcar (lambda (x) (format "%02x" (- 255 x))) rgb) "" )))
+ ))
+
+
+(defun color-pick-replace (startpoint endpoint color)
+  (let ((newcolor (color-pick color)))
+    (if (> (length newcolor) 0)
+        (progn 
+          (delete-region startpoint endpoint)
+          (goto-char startpoint)
+
+          (insert newcolor))))
+      )
+  
+(defun color-pick (color)
+  (with-temp-buffer
+    (progn
+      (if (stringp color) (setq color (list color)))
+      (apply `process-file colorpicker--script (append (list nil '(t nil) nil) color))
+      (goto-char (point-min))
+      (buffer-substring-no-properties (point) (line-end-position)))))
+
+(defun picky ()
+  (interactive)
+  (let (startpoint)
+    (save-excursion
+    
+    (skip-chars-backward "-a-z0-9,(#")
+    (setq startpoint (point))
+    
+    (if
+        (re-search-forward
+         "rgb( *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\) *)"
+         (+ (point) 20) t)
+
+        (progn
+         (color-pick-replace (+ startpoint 4) (- (point) 1) (list
+                             (match-string-no-properties 1)
+                             (match-string-no-properties 2)
+                             (match-string-no-properties 3)))))
+    
+    (if
+        (re-search-forward
+         "rgba( *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\) *, *\\([0-9]\\{1,3\\}\\),.*\)"
+         (+ (point) 20) t)
+
+        (progn
+         (color-pick-replace (+ startpoint 5) (match-end 3) (list
+                             (match-string-no-properties 1)
+                             (match-string-no-properties 2)
+                             (match-string-no-properties 3)))))
+    
+    (if
+        (re-search-forward
+         "#[abcdef[:digit:]]\\{6\\}"
+         (+ (point) 8) t)
+        
+        (progn
+          (color-pick-replace startpoint (point) (match-string-no-properties 0))))
+    
+    )))
+
+(defun pickerhook ()
+  (interactive)
+  (color-colors))
+
+
+
+(define-minor-mode color-picker-mode
+  "Syntax color css colors and provide a color picker."
+  nil " colorPick" nil
+  (xah-syntax-color-rgba)
+  (xah-syntax-color-rgb)
+  (xah-syntax-color-hex)
+  (xah-syntax-color-hsl)
+  (local-set-key (kbd "C-p") 'picky)
+  )
 
 (provide 'colorpicker)
 
--- script/colorpicker.py
+++ script/colorpicker.py
@@ -6,14 +6,25 @@ from gi.repository import Gdk
 
 picker = Gtk.ColorSelectionDialog("Color Picker")
 
+rgb = True
+
 if len(sys.argv) >= 2:
     color = Gdk.color_parse(sys.argv[1])
+    
     if color:
+        rgb = False
         picker.get_color_selection().set_current_color(color)
-
+        
+    if len(sys.argv) > 3:
+        picker.get_color_selection().set_current_color(Gdk.Color(red=int(sys.argv[1]) * 257, green=int(sys.argv[2]) * 257, blue=int(sys.argv[3]) * 257))
+        
 if picker.run() == getattr(Gtk, 'RESPONSE_OK', Gtk.ResponseType.OK):
+    
     color = picker.get_color_selection().get_current_color()
     r, g, b = [int(c / 256) for c in [color.red, color.green, color.blue]]
-    print("#{:02x}{:02x}{:02x}".format(r, g, b).upper())
+    if rgb:
+        print(str(r) + "," + str(g) + "," +  str(b))
+    else:
+        print("#{:02x}{:02x}{:02x}".format(r, g, b).upper())
 
 picker.destroy()
